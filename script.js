31384c2910464a6786a68c29f27afea96d7d5680
srhqmp <ballanosarah@gmail.com>
2024-08-21 14:23:13 +0800

    feat: add sketch area and hover effect

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const sketchArea = document.querySelector(".sketch-area");
+
+let x = 16;
+let y = 16;
+
+function createSquare() {
+  const square = document.createElement("div");
+  square.className = "square";
+  return square;
+}
+
+for (let i = 0; i < y; i++) {
+  const row = document.createElement("div");
+  row.className = "row";
+  sketchArea.appendChild(row);
+
+  for (let j = 0; j < x; j++) {
+    const square = createSquare();
+    row.appendChild(square);
+  }
+}
+
+const square = document.querySelectorAll(".square").forEach((div) => {
+  div.addEventListener("mouseover", (event) => {
+    div.style.backgroundColor = "black";
+  });
+});
