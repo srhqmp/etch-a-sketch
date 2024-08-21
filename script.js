4b5d2e13b38188f851648d65b5de71b96904c421
srhqmp <ballanosarah@gmail.com>
2024-08-21 15:37:31 +0800

    feat: randomize color and implement progressive darkening effect

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const sketchArea = document.querySelector(".sketch-area");
 const adjustButton = document.querySelector(".adjust-button");
 const resetButton = document.querySelector(".reset-button");
+const changeColorButton = document.querySelector(".change-color-button");
+
+let isColored = false;
+
+function generateRandomRGB() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 function createSquare() {
   const square = document.createElement("div");
@@ -11,7 +21,12 @@ function createSquare() {
 function addHoverEffect() {
   document.querySelectorAll(".square").forEach((div) => {
     div.addEventListener("mouseover", (event) => {
-      div.style.backgroundColor = "black";
+      if (div.style.backgroundColor) {
+        div.style.opacity = parseFloat(div.style.opacity) + 0.1;
+      } else {
+        div.style.backgroundColor = isColored ? generateRandomRGB() : "#000000";
+        div.style.opacity = 0.1;
+      }
     });
   });
 }
@@ -55,6 +70,13 @@ adjustButton.addEventListener("click", (event) => {
 resetButton.addEventListener("click", () => {
   resetSketchArea();
   generateGrids(16);
+});
+
+changeColorButton.addEventListener("click", () => {
+  changeColorButton.textContent = isColored
+    ? "change to colored pen"
+    : "change to black&white pen";
+  isColored = !isColored;
 });
 
 generateGrids(16);
